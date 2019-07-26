8b4fc7d4f753a60b21f89d93e1bf2142ee83a869
henry <liujinlong@yit.com>
2019-07-26 11:56:59 +0800

    test

---
+++ recognize.py
@@ -0,0 +1,24 @@
+import face_recognition
+import cv2
+import time
+
+known_image = face_recognition.load_image_file('lib/raw.jpg')
+cv2.imshow('known_image', known_image)
+
+known_face = face_recognition.face_encodings(known_image)[0]
+
+unknown_image = face_recognition.load_image_file('lib/test.jpg')
+
+unknown_face = face_recognition.face_encodings(unknown_image)[0]
+
+results = face_recognition.compare_faces([known_face], unknown_face)
+
+if results[0]:
+    print('yes')
+else:
+    print('no')
+
+count = 20
+while count > 0:
+    time.sleep(1)
+    count -= 1
